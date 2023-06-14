1eef65cd596b065ecd9156da4dd63ff4cfc6e764
edgar-ohanyan <edgarohanyanwork@gmail.com>
2023-06-14 13:57:24 +0400

    apgraded header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Footer from './components/Footer';
-import Home from './components/Home'
-import Header from './components/Header'
+import "./App.css";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div className="sticky-header">
+        <Header />
+      </div>
       <Home />
       <Footer />
     </div>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import Popup from "reactjs-popup";
 import "./header.css";
 import { RxHamburgerMenu } from "react-icons/rx";
+import Modal from "@mui/material/Modal";
 
 import logo from "../../assets/icons/thecollaborative-02.png";
 import tca from "../../assets/icons/thecollaborative-03.png";
 
-
-
 function Header() {
   const [showBurger, setShowBurger] = useState(false);
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+  const handleClose = () => setOpen(false);
+
   useEffect(() => {
     window.innerWidth <= 820 && setShowBurger(true);
   });
@@ -25,30 +29,63 @@ function Header() {
       </div>
       <div className="header-right">
         {showBurger ? (
-          <>
-          <RxHamburgerMenu size={40} className="header-burger" />
-          </>
+          <RxHamburgerMenu
+            size={40}
+            className="header-burger"
+            onClick={() => setOpen((o) => !o)}
+          />
         ) : (
-          <>
-            <nav className="header-nav">
-              <ul className="header-nav-list">
-                <li className="header-nav-item">
-                  <a href="#">Submit CV</a>
-                </li>
-                <li className="header-nav-item">
-                  <a href="#">Students</a>
-                </li>
-                <li className="header-nav-item">
-                  <a href="#">Recruiment</a>
-                </li>
-                <li className="header-nav-item">
-                  <a href="#">Partnership</a>
-                </li>
-              </ul>
-            </nav>
-          </>
+          <nav className="header-nav">
+            <ul className="header-nav-list">
+              <li className="header-nav-item">
+                <a href="#">Submit CV</a>
+              </li>
+              <li className="header-nav-item">
+                <a href="#">Students</a>
+              </li>
+              <li className="header-nav-item">
+                <a href="#">Recruitment</a>
+              </li>
+              <li className="header-nav-item">
+                <a href="#">Partnership</a>
+              </li>
+            </ul>
+          </nav>
         )}
       </div>
+
+      {open && (
+        <div className="header-nav-popup" ref={ref}>
+          {/* Content of the popup */}
+        </div>
+      )}
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div className="header-nav-popup" ref={ref}>
+          {/* Content of the popup */}
+          <nav className="header-nav">
+            <ul className="header-nav-list-popup">
+              <li className="header-nav-item-popup">
+                <a href="#">Submit CV</a>
+              </li>
+              <li className="header-nav-item-popup">
+                <a href="#">Students</a>
+              </li>
+              <li className="header-nav-item-popup">
+                <a href="#">Recruitment</a>
+              </li>
+              <li className="header-nav-item-popup">
+                <a href="#">Partnership</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Modal>
     </header>
   );
 }
