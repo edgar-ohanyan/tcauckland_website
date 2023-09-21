b5d55063769581c352ef712713247994ea5fbc35
edgar-ohanyan <edgarohanyan1994@gmail.com>
2023-09-21 14:38:13 +0400

    added Apply Job page functional, changed UI of it.

--- src/components/FindAJob/index.jsx
+++ src/components/FindAJob/index.jsx
@@ -22,7 +22,6 @@ const useStyles = makeStyles((theme) => ({
   formControl: {
     color: red,
     minWidth: 240,
-    marginBottom: theme.spacing(2),
   },
   select: {
     padding: theme.spacing(1),
@@ -81,7 +80,8 @@ export default function FindAJob() {
     (async () => {
       try {
         const response = await client.getEntries();
-        setEntries(response.items);
+        const filteredResponce = response.items.filter(item => "type" in item.fields);
+        setEntries(filteredResponce);
       } catch (error) {
         console.error("Error fetching entries:", error);
       }
@@ -141,7 +141,7 @@ export default function FindAJob() {
       <div className="locationSelectTop">
         <h1 className="page-title">Find A Job</h1>
         <div className="locationSelect">
-          <div className="inputSectionItem" style={{ marginBottom: 40 }}>
+          <div className="inputSectionItem">
             <FormControl className={classes.formControl}>
               <TextField
                 select
@@ -181,17 +181,13 @@ export default function FindAJob() {
                 <p className="jobFieldText">{entry.fields.location}</p>
               </div>
               <div className="jobField">
-                <p className="jobFieldTitle">Category:&nbsp;</p>
-                <p className="jobFieldText">{entry.fields.category}</p>
-              </div>
-              <div className="jobField">
                 <p className="jobFieldTitle">Type:&nbsp;</p>
                 <p className="jobFieldText">{entry.fields.type}</p>
               </div>
               <div className="jobDescription">
                 <p className="jobDescriptionText">
                   <span className="jobFieldTitle">Description:&nbsp;</span>
-                  {entry.fields.description}
+                  {entry.fields.shortDescription}
                 </p>
               </div>
             </div>
